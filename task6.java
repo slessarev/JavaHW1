0c727ce5b1d12d2a039f54e9c1f5e8955dc46467
Aleksandr Slesarev <slessarevalex@ya.ru>
2023-03-29 14:13:11 +0300

    Решил шестую задачу

---
+++ task6.java
@@ -0,0 +1,24 @@
+// 1. Задать целочисленный массив, состоящий из элементов 0 и 1.
+//  * Например: [ 1, 1, 0, 0, 1, 0, 1, 1, 0, 0 ]. С помощью цикла и условия заменить 0 на 1, 1 на 0;
+public class task6 {
+    private static int[] changeArray(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == 1) arr[i] = 0;
+            else arr[i] = 1;
+        }
+        return arr;
+    }
+    public static void main(String[] args) {
+        int[] arr = {1, 1, 0, 0, 1, 0, 1, 1, 0, 0};
+        for (int i : arr) {
+            System.out.print(i);
+        }
+
+        System.out.println();
+        int[] newArr = changeArray(arr);
+        for (int i : arr) {
+            System.out.print(i);
+        }
+        
+    }
+}
