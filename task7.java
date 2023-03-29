045ecb7dfa3702f16cae19893adc671ca75a380d
Aleksandr Slesarev <slessarevalex@ya.ru>
2023-03-29 14:26:47 +0300

    Решил седьмую задачу

---
+++ task7.java
@@ -0,0 +1,11 @@
+// Задать массив [ 1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1 ] пройти по нему циклом, и числа меньшие 6 умножить на 2;
+public class task7 {
+    public static void main(String[] args) {
+        int[] arr = {1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1};
+        System.out.println("1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1");
+        for (int i=0; i<arr.length; i++) {
+            if (arr[i]<6) arr[i] = arr[i]*2;
+            System.out.print(arr[i] + ", ");
+        }
+    }
+}
