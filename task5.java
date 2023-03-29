6691304c775bae3a84a4aad3c0c2c42a75c647a1
Aleksandr Slesarev <slessarevalex@ya.ru>
2023-03-29 14:00:54 +0300

    Решил пятую задачу

--- task5.java
+++ task5.java
@@ -2,16 +2,17 @@ public class task5 {
     private static int[] createArray(int len, int initialValue) {
         // должен вернуть массив длины len, каждое значение которого равно initialValue
         int[] arr = new int[len];
-        for (int i = 0; i<len; i++) {
+        for (int i = 0; i < len; i++) {
             arr[i] = initialValue;
         }
         return arr;
     }
+
     public static void main(String[] args) {
         int[] arr = createArray(5, 10);
-        for (int i = 0; i<arr.length; i++) {
+        for (int i = 0; i < arr.length; i++) {
             System.out.println(arr[i]);
         }
-        
+
     }
 }
